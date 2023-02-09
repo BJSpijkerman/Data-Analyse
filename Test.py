10c5eb3db6b512edc28b5f429a0ad3152a78335a
BJSpijkerman <Bram_spijkerman@outlook.com>
2023-02-09 17:03:22 +0100

    Update Test.py bevat basic stats, normTest, bosplot, Coeff. of var

--- Test.py
+++ Test.py
@@ -8,8 +8,29 @@ Created on Thu Feb  9 14:50:50 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 
-magnit = pd.read_csv('Magnit.B.V.csv', encoding='utf-8')
-Strongh = pd.read_csv('Strongh B.V.csv', encoding='utf-8')
+magnitDat = pd.read_csv('Magnit.B.V.csv', encoding='utf-8')
+stronghDat = pd.read_csv('Strongh B.V.csv', encoding='utf-8')
 
- 
+magnit = magnitDat['Magnetische fluxdichtheid [T]']
+index_magnit = magnitDat['Meting [#]']
+strongh = stronghDat['Magnetische fluxdichtheid [T]']
+index_strongh = stronghDat['Meting [#]']
+
+DescriptionMagnit = stats.describe(magnit)
+DescriptionStrongh = stats.describe(strongh)
+
+P_value_magnit = stats.normaltest(magnit)
+P_value_strongh = stats.normaltest(strongh)
+
+CoefficientOfVariance_magnit = stats.variation(magnit)
+CoefficientOfVariance_strongh = stats.variation(strongh)
+
+fig, ([ax, ax1], [ax2, ax3]) = plt.subplots(nrows=2, ncols=2)
+
+ax.boxplot(magnit)
+ax1.boxplot(strongh)
+
+ax2.scatter(index_magnit, magnit)
+ax3.scatter(index_strongh, strongh)
