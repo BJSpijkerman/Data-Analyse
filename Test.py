d5b94eab3e4d4b32ea4d0ba1cc8b7e5a391cb378
BJSpijkerman <Bram_spijkerman@outlook.com>
2023-02-09 16:16:25 +0100

    Opschoning data en uitlezen

--- Test.py
+++ Test.py
@@ -5,3 +5,11 @@ Created on Thu Feb  9 14:50:50 2023
 @author: brams
 """
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+magnit = pd.read_csv('Magnit.B.V.csv', encoding='utf-8')
+Strongh = pd.read_csv('Strongh B.V.csv', encoding='utf-8')
+
+ 
